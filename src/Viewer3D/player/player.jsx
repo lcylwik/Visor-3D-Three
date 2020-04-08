704e7857cc3f9c4e810cd06ebe9b91c5658cab79
lcylwik <cylwiklianet@gmail.com>
2020-04-08 14:40:56 -0500

    add next and prev buttons

--- src/Viewer3D/player/player.jsx
+++ src/Viewer3D/player/player.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styles from './player.module.css';
 import play from '../assets/play.svg';
-import stop from '../assets/stop.svg'
+import stop from '../assets/stop.svg';
+import next from '../assets/next.svg';
+import prev from '../assets/before.svg';
 
-const Player = ({ data, changeStep }) => {
+const Player = ({ data, onPlay, onStop, onPrev, onNext }) => {
   return (
     <div className={styles.PlayerContainer}>
-          <div className={styles.Butons}>
-              <div className={styles.Play}><img src={play}/></div>
-              <div className={styles.Stop}></div>
-          </div>
+      <div className={styles.Butons}>
+        <div onClick={(e => onPrev())} className={styles.Prev}><img alt="prev" src={prev} /></div>
+        <div onClick={(e => onPlay())} className={styles.Play}><img alt="play" src={play} /></div>
+        <div onClick={(e => onStop())} className={`${styles.Stop} ${styles.Hidden}`}><img alt="stop" src={stop} /></div>
+        <div onClick={(e => onNext())} className={styles.Next}><img alt="next" src={next} /></div>
+      </div>
     </div>
   );
 }
