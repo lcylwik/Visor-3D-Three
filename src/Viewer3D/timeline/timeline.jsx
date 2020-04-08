0c34484ef260d043d46992d31cbf068fb743ccf9
lcylwik <cylwiklianet@gmail.com>
2020-04-08 11:20:14 -0500

    skeleton timeline component

---
+++ src/Viewer3D/timeline/timeline.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './timeline.module.css';
+import {  } from '../controls/controllerSetup';
+
+const TimeLine = ({ data }) => {
+  return (
+      <div className={styles.TimeLineContainer}>
+        
+      </div>
+    );
+}
+
+export default TimeLine;
