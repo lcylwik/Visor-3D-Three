ae89711ed51df1d62fb00b5e9cb848f3f87c4540
lcylwik <cylwiklianet@gmail.com>
2020-04-08 19:33:32 -0500

    delete file no used

--- src/Viewer3D/index.jsx
+++ src/Viewer3D/index.jsx
@@ -158,7 +158,6 @@ class Viewer extends Component {
 
   animate = () => {
     requestAnimationFrame(this.animate);
-    this.controls.update();
     this.renderer.render(this.scene, this.camera);
     TWEEN.update()
   }
